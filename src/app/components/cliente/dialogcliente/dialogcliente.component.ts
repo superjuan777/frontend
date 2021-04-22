b22ec0555c0df0737827c469ed33612c0aea5ce1
superjuan777 <75589019+superjuan777@users.noreply.github.com>
2021-04-22 17:01:35 -0500

    Update dialogcliente.component.ts

--- src/app/components/cliente/dialogcliente/dialogcliente.component.ts
+++ src/app/components/cliente/dialogcliente/dialogcliente.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { Subscription } from 'rxjs';
 import { ClienteService } from 'src/app/services/cliente.service';
 
 @Component({
@@ -9,14 +10,35 @@ import { ClienteService } from 'src/app/services/cliente.service';
   templateUrl: './dialogcliente.component.html',
   styleUrls: ['./dialogcliente.component.css']
 })
-export class DialogclienteComponent implements OnInit {
+
+
+export class DialogclienteComponent implements OnInit, OnDestroy {
+
+
+  activado = true;
+
+  por = 50;
+
+  suscription: Subscription;
 
   constructor(public dialogRef: MatDialogRef<DialogclienteComponent>,
               public clienteService: ClienteService,
-              public snackBar: MatSnackBar) { }
+              public snackBar: MatSnackBar) {  }
 
   ngOnInit(): void {
+
+    this.suscription = this.clienteService.refresh$.subscribe(() => {
+      this.getClientes();
+    })
   }
+
+  ngOnDestroy() {
+    this.suscription.unsubscribe();
+  }
+
+
+
+
 
   getClientes(){
     this.clienteService.getClientes().subscribe(
@@ -26,9 +48,17 @@ export class DialogclienteComponent implements OnInit {
       },
       err => console.error(err)
     );
-}
+   }
 
- addCliente(form: NgForm){
+
+  addCliente(form: NgForm){
+
+      if(form.value.pagada === 'True'){
+        this.activado = false;
+      } else {
+        this.activado = true;
+      }
+
     var menp = " le informamos que el estado de su cuenta se encuentra en Primer Recordatorio por favor cancelar por valor de ";
     var mens = " le informamos que el estado de su cuenta se encuentra en Segundo Recordatorio por favor cancelar por valor de ";
     var mend = " le informamos que el estado de su cuenta se encuentra en mora y por consiguiente sera Desactivada por favor cancelar por valor de ";
@@ -37,98 +67,123 @@ export class DialogclienteComponent implements OnInit {
     var estado = form.value.estado;
     var pagada = form.value.pagada;
 
-    if(confirm('Estas seguro de enviar correo electronico al cliente y cambiar estado factura')){
-      if(estado === 'Primer Recordatorio'){
-        form.value.estado = 'Segundo Recordatorio';
-        form.value.mensaje = mens;
-        this.clienteService.putCliente(form.value).subscribe(
-          res => console.log(res),
-          err => console.error(err),
-        );
-        this.clienteService.createCliente(form.value).subscribe(
-          res => console.log(res),
-          err => console.error(err)
-        );
-        this.snackBar.open('Correo electronico enviado usuario informando el estado de su cuenta se encuentra en Segundo Recordatorio por favor cancelar', 'Cerrar', {
-          duration: 10000,
-          horizontalPosition: 'center',
-          verticalPosition: 'top'
-        });
+    const cero = 0;
+    const cien = 100;
+
+    var iva = 0;
+    var sub = 0;
+    var tot = 0;
+
+
+    if(form.value.totalfactura === 0){
+      this.snackBar.open('El valor de total de la factura debe ser mayor que cero para enviar correo', 'Cerrar', {
+        duration: 10000,
+        horizontalPosition: 'center',
+        verticalPosition: 'top'
+      });
+    } else {
+      if(confirm('Estas seguro de enviar correo electronico al cliente y cambiar estado factura')){
+        if(estado === 'Primer Recordatorio'){
+          form.value.estado = 'Segundo Recordatorio';
+          form.value.mensaje = mens;
+          this.clienteService.putCliente(form.value).subscribe(
+            res => console.log(res),
+            err => console.error(err),
+          );
+          this.clienteService.createCliente(form.value).subscribe(
+            res => console.log(res),
+            err => console.error(err)
+          );
+          this.snackBar.open('Correo electronico enviado usuario informando el estado de su cuenta se encuentra en Segundo Recordatorio por favor cancelar', 'Cerrar', {
+            duration: 10000,
+            horizontalPosition: 'center',
+            verticalPosition: 'top'
+          });
+          this.getClientes();
+          this.close();
+
+
+        } else if (estado === 'Segundo Recordatorio'){
+          form.value.estado = 'Usuario Desactivado';
+          form.value.mensaje = mend;
+          this.clienteService.putCliente(form.value).subscribe(
+            res => console.log(res),
+            err => console.error(err),
+          );
+          this.clienteService.createCliente(form.value).subscribe(
+            res => console.log(res),
+            err => console.error(err)
+          );
+          this.snackBar.open('Correo electronico enviado informando al usuario que sera Desactivada su cuenta por favor cancelar', 'Cerrar', {
+            duration: 10000,
+            horizontalPosition: 'center',
+            verticalPosition: 'top'
+          });
+          this.getClientes();
+          this.close();
+
+        }
+        else if (estado === 'Usuario Desactivado' && pagada === 'False'){
+          form.value.fechapago = new Date();
+          form.value.estado = 'Usuario Activo';
+          form.value.pagada = 'True';
+          form.value.mensaje = mena;
+          form.value.totalfactura = cero;
+          form.value.iva = cero;
+          form.value.subtotal = cero;
+          this.clienteService.putCliente(form.value).subscribe(
+            res => console.log(res),
+            err => console.error(err),
+          );
+          this.clienteService.createCliente(form.value).subscribe(
+            res => console.log(res),
+            err => console.error(err)
+          );
+          this.snackBar.open('Correo electronico enviado informando al usuario que sera Activada su cuenta', 'Cerrar', {
+            duration: 10000,
+            horizontalPosition: 'center',
+            verticalPosition: 'top'
+          });
+          this.getClientes();
+          this.close();
+
+        }
+        else if (estado === 'Usuario Activo' && pagada === 'True'){
+          form.value.estado = 'Primer Recordatorio';
+          form.value.pagada = 'False';
+          form.value.mensaje = menp;
+          tot = form.value.totalfactura;
+          iva = (tot * this.por)/cien;
+          sub = tot - iva;
+          tot = iva + sub;
+          if(tot === 0){
+            tot = 100;
+          }
+          form.value.totalfactura = tot;
+          form.value.iva = iva;
+          form.value.subtotal = sub;
+          this.clienteService.putCliente(form.value).subscribe(
+            res => console.log(res),
+            err => console.error(err),
+          );
+          this.clienteService.createCliente(form.value).subscribe(
+            res => console.log(res),
+            err => console.error(err)
+          );
+          this.snackBar.open('Correo electronico enviado usuario informando el estado de su cuenta se encuentra Primer Recordatorio por favor cancelar', 'Cerrar', {
+            duration: 10000,
+            horizontalPosition: 'center',
+            verticalPosition: 'top',
+          });
+          this.getClientes();
+          this.close();
+
+        }
         this.getClientes();
-        this.close();
-
-
-      } else if (estado === 'Segundo Recordatorio'){
-        form.value.estado = 'Usuario Desactivado';
-        form.value.mensaje = mend;
-        this.clienteService.putCliente(form.value).subscribe(
-          res => console.log(res),
-          err => console.error(err),
-        );
-        this.clienteService.createCliente(form.value).subscribe(
-          res => console.log(res),
-          err => console.error(err)
-        );
-        this.snackBar.open('Correo electronico enviado informando al usuario que sera Desactivada su cuenta por favor cancelar', 'Cerrar', {
-          duration: 10000,
-          horizontalPosition: 'center',
-          verticalPosition: 'top'
-        });
-        this.getClientes();
-        this.close();
-
       }
-      else if (estado === 'Usuario Desactivado' && pagada === 'False'){
-        form.value.fechapago = new Date();
-        form.value.estado = 'Usuario Activo';
-        form.value.pagada = 'True';
-        form.value.mensaje = mena;
-        form.value.totalfactura = "0";
-        form.value.iva = "0";
-        form.value.subtotal = "0";
-        this.clienteService.putCliente(form.value).subscribe(
-          res => console.log(res),
-          err => console.error(err),
-        );
-        this.clienteService.createCliente(form.value).subscribe(
-          res => console.log(res),
-          err => console.error(err)
-        );
-        this.snackBar.open('Correo electronico enviado informando al usuario que sera Activada su cuenta', 'Cerrar', {
-          duration: 10000,
-          horizontalPosition: 'center',
-          verticalPosition: 'top'
-        });
-        this.getClientes();
-        this.close();
-
-      }
-      else if (estado === 'Usuario Activo' && pagada === 'True'){
-        form.value.estado = 'Primer Recordatorio';
-        form.value.pagada = 'False';
-        form.value.mensaje = menp;
-        form.value.totalfactura = '19.278';
-        form.value.iva = "3.078";
-        form.value.subtotal = "16.200";
-        this.clienteService.putCliente(form.value).subscribe(
-          res => console.log(res),
-          err => console.error(err),
-        );
-        this.clienteService.createCliente(form.value).subscribe(
-          res => console.log(res),
-          err => console.error(err)
-        );
-        this.snackBar.open('Correo electronico enviado usuario informando el estado de su cuenta se encuentra Primer Recordatorio por favor cancelar', 'Cerrar', {
-          duration: 10000,
-          horizontalPosition: 'center',
-          verticalPosition: 'top',
-        });
-        this.getClientes();
-        this.close();
-
-      }
-      this.getClientes();
     }
+
+
   }
 
   close(){
