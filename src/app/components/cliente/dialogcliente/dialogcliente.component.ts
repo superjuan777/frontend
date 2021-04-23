ea296d2386ac4ce74c528a869b66535bfa290066
superjuan777 <75589019+superjuan777@users.noreply.github.com>
2021-04-23 18:46:09 -0500

    Update dialogcliente.component.ts

--- src/app/components/cliente/dialogcliente/dialogcliente.component.ts
+++ src/app/components/cliente/dialogcliente/dialogcliente.component.ts
@@ -15,32 +15,26 @@ import { ClienteService } from 'src/app/services/cliente.service';
 export class DialogclienteComponent implements OnInit, OnDestroy {
 
 
-  activado = true;
+   activado = true;
 
-  por = 19;
+   suscription: Subscription;
 
-  suscription: Subscription;
-
-  constructor(public dialogRef: MatDialogRef<DialogclienteComponent>,
+   constructor(public dialogRef: MatDialogRef<DialogclienteComponent>,
               public clienteService: ClienteService,
               public snackBar: MatSnackBar) {  }
 
-  ngOnInit(): void {
+   ngOnInit(): void {
 
     this.suscription = this.clienteService.refresh$.subscribe(() => {
       this.getClientes();
     })
-  }
+   }
 
-  ngOnDestroy() {
+   ngOnDestroy() {
     this.suscription.unsubscribe();
-  }
+   }
 
-
-
-
-
-  getClientes(){
+   getClientes(){
     this.clienteService.getClientes().subscribe(
       res => {
         console.log(res);
@@ -51,7 +45,37 @@ export class DialogclienteComponent implements OnInit, OnDestroy {
    }
 
 
-  addCliente(form: NgForm){
+   pago(form: NgForm){
+    if(confirm('Estas que la factura esta pagada ')){
+      form.value.fechapago = new Date();
+     form.value.estado = 'Usuario Activo';
+     form.value.pagada = 'True';
+     form.value.mensaje = " le informamos que su cuenta fue Activada"
+     form.value.totalfactura = 0;
+     form.value.iva = 0;
+     form.value.subtotal = 0;
+     this.clienteService.putCliente(form.value).subscribe(
+      res => console.log(res),
+      err => console.error(err),
+     );
+     this.clienteService.createCliente(form.value).subscribe(
+      res => console.log(res),
+      err => console.error(err)
+     );
+     this.snackBar.open('Correo electronico enviado informando al usuario que sera Activada su cuenta', 'Cerrar', {
+      duration: 10000,
+      horizontalPosition: 'center',
+      verticalPosition: 'top'
+     });
+     this.getClientes();
+     this.close();
+    }
+   }
+
+
+   addCliente(form: NgForm){
+    const cero = 0;
+    const cien = 100;
 
       if(form.value.pagada === 'True'){
         this.activado = false;
@@ -59,16 +83,21 @@ export class DialogclienteComponent implements OnInit, OnDestroy {
         this.activado = true;
       }
 
+      tot = form.value.totalfactura;
+          iva = (tot * por)/cien;
+          sub = tot - iva;
+          tot = iva + sub;
+
+
     var menp = " le informamos que el estado de su cuenta se encuentra en Primer Recordatorio por favor cancelar por valor de ";
     var mens = " le informamos que el estado de su cuenta se encuentra en Segundo Recordatorio por favor cancelar por valor de ";
     var mend = " le informamos que el estado de su cuenta se encuentra en mora y por consiguiente sera Desactivada por favor cancelar por valor de ";
-    var mena = " le informamos que su cuenta sera Activada";
+    var mena = " le informamos que su cuenta fue Activada";
 
     var estado = form.value.estado;
     var pagada = form.value.pagada;
+    var por = form.value.por;
 
-    const cero = 0;
-    const cien = 100;
 
     var iva = 0;
     var sub = 0;
@@ -146,14 +175,13 @@ export class DialogclienteComponent implements OnInit, OnDestroy {
           });
           this.getClientes();
           this.close();
-
         }
         else if (estado === 'Usuario Activo' && pagada === 'True'){
           form.value.estado = 'Primer Recordatorio';
           form.value.pagada = 'False';
           form.value.mensaje = menp;
           tot = form.value.totalfactura;
-          iva = (tot * this.por)/cien;
+          iva = (tot * por)/cien;
           sub = tot - iva;
           tot = iva + sub;
           if(tot === 0){
@@ -184,10 +212,10 @@ export class DialogclienteComponent implements OnInit, OnDestroy {
     }
 
 
-  }
+   }
 
-  close(){
+   close(){
     this.dialogRef.close();
-  }
+   }
 
 }
